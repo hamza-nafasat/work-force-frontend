6ceeddfbf0521354e33fa56414acd7e547d418af
hamza-nafasat <hamzanafasat4155@gmail.com>
2024-10-01 22:18:11 +0500

    add some changes

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,12 +3,9 @@ import { authApiPoint } from "./api/authApi";
 
 const store = configureStore({
   reducer: {
-
     [authApiPoint.reducerPath]: authApiPoint.reducer,
-
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat([authApiPoint.middleware]),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([authApiPoint.middleware]),
 });
 
 export default store;
